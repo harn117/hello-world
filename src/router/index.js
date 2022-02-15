8d2a33e10e4724c3b46d08d612159d549b79a686
david.neira <david.neira@arkdia.co>
2022-02-15 09:51:32 -0500

    up

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Login from '../components/Login.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  { path: "/login", name: 'Login', component: Login },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template class="container">
+  <div class="container">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Prueba de desarrollo Vue y Laravel"/>
+  </div>
+
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/Word.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
